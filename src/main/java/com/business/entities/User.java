9d6ab821eee7279de15a8c68e40f4a394cf186c2
deepakmn03 <deepakmn@LAPTOP-JFJ8H8M8.>
2025-11-10 19:49:15 +0000

    added postgres db details

--- src/main/java/com/business/entities/User.java
+++ src/main/java/com/business/entities/User.java
@@ -12,8 +12,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="users")
 public class User 
 {
 	@Id
